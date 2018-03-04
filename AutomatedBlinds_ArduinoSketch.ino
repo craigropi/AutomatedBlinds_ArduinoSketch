33c49e973dc7d3e38cc61c7a352ab5434f3d6d73
craigropi <craigropi@gmail.com>
2018-03-04 00:04:15 -0500

    Added Rolling Up/Down Functionality
    Blinds can now be rolled up/down in addition to just being nudged.  The arduino keeps track of the blinds current position and recalibrates using the limit switch at the top of the blinds as a home sensor.

--- AutomatedBlinds_ArduinoSketch.ino
+++ AutomatedBlinds_ArduinoSketch.ino
@@ -3,23 +3,36 @@
 #include <CurieTimerOne.h>
 
 //Clockwise rotor movement moves the blinds down.
+//In this file, 'roll' menas roll the blinds all the way up/down, nudge means move them a small distance.
 BLEPeripheral blePeripheral;  // BLE Peripheral Device (the board you're programming)
 BLEService blindsService("19B10000-E8F2-537E-4F6C-D104768A1214"); // BLE LED Service
 
 // BLE Blinds Characteristic - custom 128-bit UUID, read and writable by central
 BLEUnsignedCharCharacteristic blindsCharacteristic("19B10001-E8F2-537E-4F6C-D104768A1214", BLERead | BLEWrite);
 
-int blindsPos = 0;    // variable to store blinds position. 0 is uncalibrated.  1 is down. 100 is fully up.  0 Is not initialized. 99 Is just below the limit switch.
+static unsigned long blindsPos = 0;    // variable to store blinds position. 0 is uncalibrated.  1 is down. 100 is fully up.  0 Is not initialized. 99 Is just below the limit switch.
 static unsigned long lastLimitSwitchTime = 0;  //stores the time of the last limit switch transition in milliseconds.
 //static unsigned long currentInterruptTime = 0;  //stores the time of the current interrupt in milliseconds
-int limitSwitchState = 0; // stores the state of the limit switch'
-int goingDown = 0; // 1 if motor is currently going down, 0 if not.
-int goingUp = 0; // 1 if motor is currently going Up, 0 if not.
-int debouncing = 0; // 1 if the 
-static unsigned long goingDownStartTime = 0; // Time in ms whent the motor stated going down.
-static unsigned long goingUpStartTime = 0; // Time in ms whent the motor stated going up.
-static unsigned long motorNudgeDownTime = 1000; // The amount of time the motor should be driven down when commanded to nudge down, in ms.
-static unsigned long motorNudgeUpTime = 1000; // The amount of time the motor should be driven down when commanded to nudge down, in ms.
+int limitSwitchState = 0; // stores the state of the limit switch.
+int debouncing = 0; // 1 if the switch has been toggled .
+
+int rollingDown = 0; // 1 if motor is currently rolling down, 0 if not.
+int rollingUp = 0; // 1 if motor is currently rolling up, 0 if not.
+int nudgingDown = 0; // 1 if motor is currently nudging down, 0 if not.
+int nudgingUp = 0; // 1 if motor is currently nudging up, 0 if not.
+
+static unsigned long nudgeDownStartTime = 0; // Time in ms when the motor started nudging down.
+static unsigned long nudgeUpStartTime = 0; // Time in ms when the motor started nudging up.
+static unsigned long rollDownStartTime = 0; // Time in ms when the motor started rolling down.
+static unsigned long rollUpStartTime = 0; // Time in ms when the motor started rolling up.
+static unsigned long motorNudgeDownTime = 500; // The amount of time the motor should be driven down when commanded to nudge down, in ms.
+static unsigned long motorNudgeUpTime = 500; // The amount of time the motor should be driven down when commanded to nudge down, in ms.
+static unsigned long motorRollDownTime = 4300; // The amount of time the motor should be driven down when commanded to roll down, in ms.
+static unsigned long motorRollUpTime = 4300; // The amount of time the motor should be driven down when commanded to roll down, in ms.
+
+static unsigned long rollDownConstant = 40; // The amount of time it takes the blinds to roll down 1%, in ms.
+static unsigned long rollUpConstant = 40; // The amount of time it takes the blinds to roll up 1%, in ms.
+
 static unsigned long debounceTime = 200; // Time the limit switch should be bounced, in ms.
 
 int interruptMessagetoBePrinted = 0; // Bitmask for messages to be printed from interrupts
@@ -57,17 +70,20 @@ void setup() {
 }
 
 void loop() {
+  //Motor Driver should always be in sleep mode if the is no BLE device connected.
   digitalWrite(4, LOW); //Put the motor driver in sleep mode
   // listen for BLE peripherals to connect:
   BLECentral central = blePeripheral.central();
   
   checkMessagetobePrinted();
-  checkGoingDown();
-  checkGoingUp();
+  checkNudgingDown();
+  checkNudgingUp();
+  checkRollingDown();
+  checkRollingUp();
   checkifNoLongerUp();
   checkifDebouncing();
   
-  // if a central is connected to peripheral:
+  // if a central is connected to BLE peripheral:
   if (central) {
     //Serial.print("Connected to central: ");
     // print the central's MAC address:
@@ -76,38 +92,59 @@ void loop() {
     // while the central is still connected to peripheral:
     while (central.connected()) {
       checkMessagetobePrinted();
-      checkGoingDown();
-      checkGoingUp();
+      checkNudgingDown();
+      checkNudgingUp();
+      checkRollingDown();
+      checkRollingUp();
       checkifNoLongerUp();
       checkifDebouncing();
       
-      // if the remote device wrote to the characteristic,
-      // use the value to control the LED:
-      if (blindsCharacteristic.written()) {
+      
+      // if the remote device wrote to the characteristic and the blinds are not in motion already,
+      // use the value set via BLE to control the blinds
+      if (blindsCharacteristic.written() && rollingDown == 0 && rollingUp == 0 && nudgingDown == 0 && nudgingUp == 0) {
         digitalWrite(4, HIGH); //Take the motor driver out of sleep mode
         limitSwitchState = digitalRead(13);
-        if (blindsCharacteristic.value()) {   // any value other than 0 "Roll Blinds Down"
-          //Serial.println(F("Blinds Down"));
+        if (blindsCharacteristic.value() == 1) {   // Value of 1 means "Nudge Blinds Down"
+          Serial.println("Nudge Blinds Down");
+          Serial.println(blindsPos);
           digitalWrite(2, LOW);  //Run the motor in the clockwise direction
+          nudgingDown = 1;
+          nudgeDownStartTime = millis();
           // Run the PWM (pin#, %ofPWM [MUST HAVE '.0' at end or this works a different way], Period in us)
-          CurieTimerOne.pwmStart(3, 80.0, 50);
-          goingDown = 1;
-          goingDownStartTime = millis();
-          //delay(1000);                       // Run the motor for 500ms
-          //CurieTimerOne.pwmStop();
-        } else if(blindsPos != 100 && limitSwitchState == HIGH){   // a 0 value "Roll Blinds Up", as long as the blinds aren't fully up already (limit switch is high and saved state is not 100)
-          //Serial.println(F("Blinds Up"));
+          CurieTimerOne.pwmStart(3, 77.0, 50);
+        
+        } else if(blindsCharacteristic.value() == 3 && blindsPos > 1){   // Value of 3 means "Roll Blinds Down" all the way
+          Serial.println("Roll Blinds Down");
+          Serial.println(blindsPos);
+          digitalWrite(2, LOW);  //Run the motor in the clockwise direction
+          rollingDown = 1;
+          rollDownStartTime = (millis() - ((100-blindsPos)*rollDownConstant)); // When rolling down, prorate the roll down by however much the blinds are already rolled down.
+          Serial.println(millis());
+          Serial.println(((100-blindsPos)*rollDownConstant));
+          // Run the PWM (pin#, %ofPWM [MUST HAVE '.0' at end or this works a different way], Period in us)
+          CurieTimerOne.pwmStart(3, 77.0, 50);
+        
+        } else if (blindsCharacteristic.value() == 0 && blindsPos != 100 && limitSwitchState == HIGH){   // Value of 0 means "Nudge Blinds Up", as long as the blinds aren't fully up already (limit switch is high and saved state is not 100)
+          Serial.println("Nudge Blinds Up");
+          Serial.println(blindsPos);
           digitalWrite(2, HIGH);  //Run the motor in the counterclockwise direction
+          nudgingUp = 1;
+          nudgeUpStartTime = millis();
           // Run the PWM (pin#, %ofPWM [MUST HAVE '.0' at end or this works a different way], Period in us)
           CurieTimerOne.pwmStart(3, 100.0, 50);
-          goingUp = 1;
-          goingUpStartTime = millis();
-          //delay(1000);                       // Run the motor for 500ms
-          //CurieTimerOne.pwmStop();
+        
+        } else if(blindsCharacteristic.value() == 2 && blindsPos != 100 && limitSwitchState == HIGH){   // a 2 value "Roll Blinds Up" all the way, as long as the blinds aren't fully up already (limit switch is high and saved state is not 100)
+          Serial.println("Roll Blinds Up");
+          Serial.println(blindsPos);
+          digitalWrite(2, HIGH);  //Run the motor in the counterclockwise direction
+          rollingUp = 1;
+          rollUpStartTime = millis();
+          // Run the PWM (pin#, %ofPWM [MUST HAVE '.0' at end or this works a different way], Period in us)
+          CurieTimerOne.pwmStart(3, 100.0, 50);
         } else {
           //Serial.println(F("Blinds were not raised, because they're already all the way up"));
         }
-        // digitalWrite(4, LOW); //Put the motor driver in sleep mode
       }
     }
   }
@@ -116,8 +153,12 @@ void loop() {
 // ISR triggered by a falling edge on pin 13 (limit switch is pressed).
 void limitPressed() {
   if (blindsPos != 100 && debouncing != 1) {
-    CurieTimerOne.pwmStop();  // Stop driving the motor's PWM
     digitalWrite(4, LOW); //Put the motor driver in sleep mode
+    CurieTimerOne.pwmStop();  // Stop driving the motor's PWM
+    nudgingDown = 0;
+    rollingDown = 0;
+    nudgingUp = 0;
+    rollingUp = 0;
     debouncing = 1;
     lastLimitSwitchTime = millis();
     blindsPos = 100;
@@ -128,20 +169,21 @@ void limitPressed() {
 }
 
 void checkifDebouncing () {
-  if (millis() - lastLimitSwitchTime > debounceTime) {
+  if (debouncing == 1 && ((millis() - lastLimitSwitchTime) > debounceTime)) {
     debouncing = 0;
+    Serial.println((String) "Debouncing disabled, blindsPos = " + blindsPos);
   }
 }
 
 // Checks if the limit switch is no longer engaged and sets blindsPos to 99 if so.
 void checkifNoLongerUp() {
-  if (blindsPos == 100 && debouncing != 1) {
+  if (blindsPos == 100 && debouncing != 1 && (rollingDown || nudgingDown)) {
     limitSwitchState = digitalRead(13);
     if (limitSwitchState == HIGH) {
+    lastLimitSwitchTime = millis();
+    debouncing  = 1;
     blindsPos = 99;
     Serial.println("Set blinds position to 99, because limit switch was released.");
-    debouncing  = 1;
-    lastLimitSwitchTime = millis();
     }
   }
 }
@@ -162,39 +204,79 @@ void checkifNoLongerUp() {
 
 void checkMessagetobePrinted() {
   if (interruptMessagetoBePrinted) {  // If there is a message to be printed
+    Serial.println((String)"interruptMessagetoBePrinted " + interruptMessagetoBePrinted);
     switch (interruptMessagetoBePrinted) { //interruptMessagetoBePrinted is a bitmask for the messages that should be printed.
       case 1:
       Serial.println(interruptMessage1);
+      break;
       case 2:
       Serial.println(interruptMessage2);
+      break;
       case 3:
       Serial.println(interruptMessage2);
       Serial.println(interruptMessage1);
+      break;
       case 4:
       Serial.println(interruptMessage4);
+      break;
       case 5:
       Serial.println(interruptMessage4);
       Serial.println(interruptMessage1);
+      break;
     }
     interruptMessagetoBePrinted = 0;
   }
 }
 
-// Checks if the motor is currently going down, and if so, stops it if it's been 'motorNudgeDownTime' since starting.
-void checkGoingDown() { 
-  if (goingDown && millis() - goingDownStartTime > motorNudgeDownTime) { 
+//String interruptMessage1 = "limitPressed Interrupt Complete";
+//String interruptMessage2 = "Interrupt set blinds position to 100.";
+//String interruptMessage4 = "Interrupt set blinds position to 99.";
+
+// Checks if the motor is currently nudging down, and if so, stops it if it's been 'motorNudgeDownTime' since starting.
+void checkNudgingDown() { 
+  if (nudgingDown && ((millis() - nudgeDownStartTime) > motorNudgeDownTime)) { 
     CurieTimerOne.pwmStop(); // Stop driving the motor's PWM
     digitalWrite(4, LOW); //Put the motor driver in sleep mode
-    goingDown = 0;
+    nudgingDown = 0;
+    if(blindsPos > motorNudgeDownTime/rollDownConstant) {
+      blindsPos -= (motorNudgeDownTime/rollDownConstant);
+    } else {
+      blindsPos = 1;
+    }
   }
 }
 
-// Checks if the motor is currently going up, and if so, stops it if it's been 'motorNudgeUpTime' since starting.
-void checkGoingUp() {
-  if (goingUp && millis() - goingUpStartTime > motorNudgeUpTime) {
+// Checks if the motor is currently rolling down, and if so, stops it if it's been 'motorRollDownTime' since starting.
+void checkRollingDown() { 
+  if (rollingDown && ((millis() - rollDownStartTime) > motorRollDownTime)) { 
     CurieTimerOne.pwmStop(); // Stop driving the motor's PWM
     digitalWrite(4, LOW); //Put the motor driver in sleep mode
-    goingUp = 0;  
+    rollingDown = 0;
+    blindsPos = 1;
+  }
+}
+
+// Checks if the motor is currently nudging up, and if so, stops it if it's been 'motorNudgeUpTime' since starting.
+void checkNudgingUp() {
+  if (nudgingUp && ((millis() - nudgeUpStartTime) > motorNudgeUpTime)) {
+    CurieTimerOne.pwmStop(); // Stop driving the motor's PWM
+    digitalWrite(4, LOW); //Put the motor driver in sleep mode
+    nudgingUp = 0;
+    if(blindsPos < (100-motorNudgeDownTime/rollDownConstant)) {
+      blindsPos += (motorNudgeUpTime/rollUpConstant);
+    } else {
+      blindsPos = 100;
+    }
+  }
+}
+
+// Checks if the motor is currently rolling up, and if so, stops it if it's been 'motorRollUpTime' since starting.
+void checkRollingUp() {
+  if (rollingUp && ((millis() - rollUpStartTime) > motorRollUpTime)) {
+    CurieTimerOne.pwmStop(); // Stop driving the motor's PWM
+    digitalWrite(4, LOW); //Put the motor driver in sleep mode
+    rollingUp = 0;
+    blindsPos = 99; // Will only get here if the limit switch is not hit.
   }
 }
 
